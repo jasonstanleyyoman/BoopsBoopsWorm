78eb33ff012201ae8ce6c4077d3f121d350aa156
ravielze <ravielz20@gmail.com>
2021-02-14 20:35:53 +0700

    BoopsBoops

---
+++ src/main/java/za/co/entelect/challenge/common/StrategyUtils.java
@@ -0,0 +1,12 @@
+package za.co.entelect.challenge.common;
+
+import za.co.entelect.challenge.entities.*;
+import za.co.entelect.challenge.Bot;
+
+public class StrategyUtils {
+    public static Worm computeTargetWorm() {
+        GameState gameState = Bot.getGameState();
+        Technologist = gameState
+        return null;
+    }
+}
